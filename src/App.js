a3d0fb0f66e1e890e32c287efbcedd5f7199978d
Hong Jin Lau, Jon <hongjinlau@gmail.com>
2020-08-29 00:42:22 +0800

    add active color for button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
 
   const [cabinet, setCabinet] = useState([]);
   const [filteredCabinet, setFilteredCabinet] = useState([]);
+  const [activeCabinetFilter, setActiveCabinetFilter] = useState('所有成员');
 
   useEffect(() => {
     fetch('/cabinet.json')
@@ -21,30 +22,32 @@ const App = () => {
   const filterCabinetMembers = (status) => {
     const filteredCabinet = cabinet.filter(member => member['Filter'] === status);
     setFilteredCabinet(filteredCabinet);
+    setActiveCabinetFilter(status);
   }
 
   const resetCabinet = () => {
     setFilteredCabinet(cabinet);
+    setActiveCabinetFilter('所有成员');
   }
 
   return (
     <div className="bg-gray-100">
-      <h1 className="text-3xl text-center my-3">2020内阁</h1>
+      <h1 className="text-3xl text-center my-3">2020内阁改组</h1>
       <div className="flex justify-center mb-4">
         <button 
-          className="border-2 border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white focus:outline-none active:bg-blue-700 p-2 mr-2 rounded"
+          className={`border-2 border-blue-500 hover:bg-blue-500 hover:text-white focus:outline-none active:bg-blue-700 p-2 mr-2 rounded ${activeCabinetFilter === '职务有变动' ? 'bg-blue-500 text-white' : 'bg-white text-blue-500'}`}
           onClick={() => filterCabinetMembers('职务有变动')}
         >
           职务有变动
         </button>
         <button 
-          className="border-2 border-green-500 text-green-500 hover:bg-green-500 hover:text-white focus:outline-none active:bg-green-700 p-2 mr-2 rounded"
+          className={`border-2 border-green-500 hover:bg-green-500 hover:text-white focus:outline-none active:bg-green-700 p-2 mr-2 rounded ${activeCabinetFilter === '职务没变' ? 'bg-green-500 text-white' : 'bg-white text-green-500'}`}
           onClick={() => filterCabinetMembers('职务没变')}
         >
           职务没变
         </button>
         <button 
-          className="border-2 border-gray-500 text-gray-500 hover:bg-gray-500 hover:text-white focus:outline-none active:bg-gray-700 p-2 mr-2 rounded"
+          className={`border-2 border-gray-500 hover:bg-gray-500 hover:text-white focus:outline-none active:bg-gray-700 p-2 mr-2 rounded ${activeCabinetFilter === '所有成员' ? 'bg-gray-500 text-white' : 'bg-white text-gray-500'}`}
           onClick={resetCabinet}
         >
           所有成员
